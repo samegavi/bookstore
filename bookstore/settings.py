c42868882d760e263e13872dcd2aa5002e691bdd
Sam Amegavi <samegavi@gmail.com>
2024-04-07 18:48:13 +0000

    updated

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -160,7 +160,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 
